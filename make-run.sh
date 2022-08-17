84b1bd837890708b46128cc299f811b2c498e604
phaffonso <pedrohaffonso@gmail.com>
2022-08-17 17:29:52 +0000

    Correções em shell script/watchdog para uso com crontab

--- make-run.sh
+++ make-run.sh
@@ -3,15 +3,17 @@
 #make sure a process is always running.
 #SET UP with cron
 
-export DISPLAY=:0 #needed if you are running a simple gui app.
+#export DISPLAY=:0 #needed if you are running a simple gui app.
 
 process=python3
 makerun="/usr/bin/python3 ${HOME}/dev/epidemic-observatory/collect_data.py"
 
-if ps ax | grep -v grep | grep $process > /dev/null
+if pgrep $process > /dev/null
 then
+    printf "Process '%s' is running.\n" "$process"
     exit
 else
+    printf "Starting process '%s' with command '%s'.\n" "$process" "$makerun"
     $makerun &
 fi
 
