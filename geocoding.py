f2ad187941a91ca2a242f400eb7f83f4fc509438
phaffonso <pedrohaffonso@gmail.com>
2022-08-29 19:22:03 +0000

    Melhoria no filtro pre-geocoding

--- geocoding.py
+++ geocoding.py
@@ -7,7 +7,9 @@ import re
 re1 = re.compile('(^| )(he|she|ele|ela|they|ella) ?[/\|\- ] ?(her|him|ella|el|they|them|dela|dele|a)', re.IGNORECASE)
 re2 = re.compile('(where|world|planet|earth|mundo)', re.IGNORECASE)
 re3 = re.compile('[0-9][0-9](\+|y)', re.IGNORECASE)
-re4 = re.compile('^\(?\+?[0-9]+\)?$', re.IGNORECASE)
+re4 = re.compile("[\(\+'][0-9]{2}\)?", re.IGNORECASE)
+re5 = re.compile('bts|enhypen|heeseung', re.IGNORECASE)
+re6 = re.compile('^[0-9]+$')
 
 # geocode_result = gmaps.geocode('Mountain View, CA')
 # print(geocode_result)
@@ -15,19 +17,27 @@ re4 = re.compile('^\(?\+?[0-9]+\)?$', re.IGNORECASE)
 
 def isLocation(text):
     return (len(text) < 50 and
-            re1.match(text) is None and
-            re2.match(text) is None and
-            re3.match(text) is None and
-            re4.match(text) is None)
+            re1.search(text) is None and
+            re2.search(text) is None and
+            re3.search(text) is None and
+            re4.search(text) is None and
+            re5.search(text) is None and
+            re6.search(text) is None)
+
+limit = 100
 
 def geocodeAndSave():
+    count = 0
     gmaps = googlemaps.Client(key=credentials_geo.API_KEY)
     for loc in storage.getUngeocodedLocations():
-        if(isLocation(loc[0]) and storage.getGeocoded(loc) is None):
+        if(isLocation(loc[0]) and storage.getGeocoded(loc[0]) is None):
             print(loc[0])
             geocode_result = gmaps.geocode(loc)
             print(geocode_result)
             storage.saveGeocoded(loc, geocode_result)
+            count += 1
             print('---saved---')
+            if(count >= limit):
+              return
 
 geocodeAndSave()
